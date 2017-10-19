ed1f484aa8c8c4811d8c63fa8e744d77fbba1982
BuildTools <xnaxdy@hydra-development.net>
2017-10-19 12:12:29 +0100

    Fix formatting

--- Student/Student.java
+++ Student/Student.java
@@ -1,8 +1,10 @@
-public class Student{
+public class Student
+{
     private String name;
     private int ucas;
     
-    public Student(String studentName, int ucasNumber){
+    public Student(String studentName, int ucasNumber)
+    {
         name = studentName;
         ucas = ucasNumber;
     }
